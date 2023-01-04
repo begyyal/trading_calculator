2f12e3eb9679f3b1c6985a682b9de0b246be9b4b
begyyal <begyyal@gmail.com>
2023-01-04 22:56:53 +0900

    init [skip ci]

--- .github/workflows/sh/tweet_template.sh
+++ .github/workflows/sh/tweet_template.sh
@@ -2,8 +2,8 @@
 
 tag_name=$1
 repo_url=$2 # https
-repos_name=""
-ext="" # hashtag etc...
+repos_name=/trading_calculator
+ext="#test"
 
 LF=$'\\n'
 text="${repos_name} updated to ${tag_name}${LF}${repo_url}"
